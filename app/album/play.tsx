2722148757db76228090266678666fec9612aec2
Revaycolizer <109684996+Revaycolizer@users.noreply.github.com>
2023-03-29 08:47:43 +0300

    Add files via upload

--- app/album/play.tsx
+++ app/album/play.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function play(){
   return (
-    <footer className='fixed absolute bottom-0 w-full h-1/4 bg-white-900 shadow-xl'>
-        <section className='fixed bg-white-900 shadow-xl'>
+    <footer className='fixed  w-full h-1/4 bg-white-900 shadow-xl'>
+        {/* <section className='fixed bg-white-900 shadow-xl'> */}
     <div>play</div>
-    </section>
+    {/* </section> */}
     </footer>
   )
 }
